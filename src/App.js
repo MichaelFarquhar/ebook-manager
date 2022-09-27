d57ddd4d78b663916aefe50a268207cf3bd86b6c
Michael <michaelfarquhar4@gmail.com>
2022-09-27 19:33:13 -0400

    Install tailwind css

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import { PDFViewer } from './components/PDFViewer.jsx';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
+        <div>
+            <header>
+                <h1 className="text-3xl font-bold">Test</h1>
                 <PDFViewer />
             </header>
         </div>
