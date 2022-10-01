2338ef0998551c66de8eab92a906648ff1631824
Michael <michaelfarquhar4@gmail.com>
2022-10-01 16:25:53 -0400

    Add top menu bar to PDF Viewer

--- src/components/PDFViewer/PDFViewerMenu.jsx
+++ src/components/PDFViewer/PDFViewerMenu.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { currentPageState, totalPagesState } from '../../atoms';
 
@@ -6,13 +7,89 @@ export const PDFViewerMenu = () => {
     const setCurrentPage = useSetRecoilState(currentPageState);
     const totalPages = useRecoilValue(totalPagesState);
 
-    const [modalOpen, setModalOpen] = useState(false);
-
     return (
-        <div>
-            <button onClick={() => setModalOpen(true)}>Bookmark</button>
-            <button onClick={() => setCurrentPage(1)}>First Page</button>
-            <button onClick={() => setCurrentPage(totalPages)}>Last Page</button>
+        <div className="bg-primary">
+            <div className="container mx-auto">
+                <div className="flex justify-between py-4 text-white">
+                    <Link className="btn btn-sm flex items-centers space-x-2" to="/">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="currentColor"
+                            className="w-6 h-6"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                d="M20.25 12a.75.75 0 01-.75.75H6.31l5.47 5.47a.75.75 0 11-1.06 1.06l-6.75-6.75a.75.75 0 010-1.06l6.75-6.75a.75.75 0 111.06 1.06l-5.47 5.47H19.5a.75.75 0 01.75.75z"
+                                clipRule="evenodd"
+                            />
+                        </svg>
+                        <span>Back</span>
+                    </Link>
+                    <div className="text-lg font-bold">filename.pdf</div>
+                    <div className="flex items-center">
+                        <div className="tooltip tooltip-bottom z-50" data-tip="Bookmark">
+                            <button className="btn btn-sm btn-ghost">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                    fill="currentColor"
+                                    className="w-6 h-6"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M6.32 2.577a49.255 49.255 0 0111.36 0c1.497.174 2.57 1.46 2.57 2.93V21a.75.75 0 01-1.085.67L12 18.089l-7.165 3.583A.75.75 0 013.75 21V5.507c0-1.47 1.073-2.756 2.57-2.93z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
+                            </button>
+                        </div>
+                        <div
+                            className="tooltip tooltip-bottom z-50"
+                            data-tip="First Page"
+                        >
+                            <button
+                                className="btn btn-sm btn-ghost"
+                                onClick={() => setCurrentPage(1)}
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                    fill="currentColor"
+                                    className="w-6 h-6"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M5.625 1.5H9a3.75 3.75 0 013.75 3.75v1.875c0 1.036.84 1.875 1.875 1.875H16.5a3.75 3.75 0 013.75 3.75v7.875c0 1.035-.84 1.875-1.875 1.875H5.625a1.875 1.875 0 01-1.875-1.875V3.375c0-1.036.84-1.875 1.875-1.875zm6.905 9.97a.75.75 0 00-1.06 0l-3 3a.75.75 0 101.06 1.06l1.72-1.72V18a.75.75 0 001.5 0v-4.19l1.72 1.72a.75.75 0 101.06-1.06l-3-3z"
+                                        clipRule="evenodd"
+                                    />
+                                    <path d="M14.25 5.25a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963A5.23 5.23 0 0016.5 7.5h-1.875a.375.375 0 01-.375-.375V5.25z" />
+                                </svg>
+                            </button>
+                        </div>
+                        <div className="tooltip tooltip-bottom z-50" data-tip="Last Page">
+                            <button
+                                className="btn btn-sm btn-ghost"
+                                onClick={() => setCurrentPage(totalPages)}
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                    fill="currentColor"
+                                    className="w-6 h-6"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M5.625 1.5H9a3.75 3.75 0 013.75 3.75v1.875c0 1.036.84 1.875 1.875 1.875H16.5a3.75 3.75 0 013.75 3.75v7.875c0 1.035-.84 1.875-1.875 1.875H5.625a1.875 1.875 0 01-1.875-1.875V3.375c0-1.036.84-1.875 1.875-1.875zm5.845 17.03a.75.75 0 001.06 0l3-3a.75.75 0 10-1.06-1.06l-1.72 1.72V12a.75.75 0 00-1.5 0v4.19l-1.72-1.72a.75.75 0 00-1.06 1.06l3 3z"
+                                        clipRule="evenodd"
+                                    />
+                                    <path d="M14.25 5.25a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963A5.23 5.23 0 0016.5 7.5h-1.875a.375.375 0 01-.375-.375V5.25z" />
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
